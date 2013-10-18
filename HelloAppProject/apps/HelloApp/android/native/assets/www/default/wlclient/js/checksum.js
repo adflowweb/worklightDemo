6950f93321861d6651521f15c2673390d829aa0d
nadir93 <nadir93@gmail.com>
2013-10-18 17:06:23 +0900

    first commit

---
+++ HelloAppProject/apps/HelloApp/android/native/assets/www/default/wlclient/js/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":1621195574,"date":1381980787227,"machine":"nadir93-macpro.local"};
+/* Date: Thu Oct 17 12:33:07 KST 2013 */
---
+++ HelloAppProject/apps/HelloApp/android/native/src/com/HelloApp/GCMIntentService.java
@@ -0,0 +1,5 @@
+package com.HelloApp;
+
+public class GCMIntentService extends com.worklight.androidgap.push.GCMIntentService{
+	//Nothing to do here...
+}
