ff4a9aa50c74956b6302feddd9d48105220eeaeb
nadir93 <nadir93@gmail.com>
2013-12-18 17:56:25 +0900

    test commit

--- ADFlowOffice/apps/ADFlowOffice/android/native/assets/www/default/wlclient/js/checksum.js
+++ ADFlowOffice/apps/ADFlowOffice/android/native/assets/www/default/wlclient/js/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":3325969986,"date":1387335686066,"machine":"nadir93-macpro.local"};
-/* Date: Wed Dec 18 12:01:26 KST 2013 */
+var WL_CHECKSUM = {"checksum":2026144822,"date":1387342866653,"machine":"nadir93-macpro.local"};
+/* Date: Wed Dec 18 14:01:06 KST 2013 */
