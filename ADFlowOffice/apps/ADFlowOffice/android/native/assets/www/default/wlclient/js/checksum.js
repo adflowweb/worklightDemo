9fd7855d7e60cc147672c9b2cc5418de47ffd0f5
nadir93 <nadir93@gmail.com>
2013-12-19 12:19:54 +0900

    delete android

--- ADFlowOffice/apps/ADFlowOffice/android/native/assets/www/default/wlclient/js/checksum.js
+++
@@ -1,2 +0,0 @@
-var WL_CHECKSUM = {"checksum":1309068079,"date":1387422072907,"machine":"nadir93-macpro.local"};
-/* Date: Thu Dec 19 12:01:12 KST 2013 */
--- ADFlowOffice/apps/ADFlowOffice/android/native/src/com/ADFlowOffice/ForegroundService.java
+++
@@ -1,5 +0,0 @@
-package com.ADFlowOffice;
-
-public class ForegroundService extends com.worklight.androidgap.WLForegroundService{
-	//Nothing to do here...
-}
