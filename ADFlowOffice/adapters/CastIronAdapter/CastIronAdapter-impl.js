e32257892c2af0d09e96869d84a02f9d79e5e20a
kicho <kicho.adflow@gmail.com>
2013-12-23 21:45:49 +0900

    Cast Iron Adapter Add.

---
+++ ADFlowOffice/adapters/CastIronAdapter/CastIronAdapter-impl.js
@@ -0,0 +1,67 @@
+/*
+ *  Licensed Materials - Property of IBM
+ *  5725-I43 (C) Copyright IBM Corp. 2011, 2013. All Rights Reserved.
+ *  US Government Users Restricted Rights - Use, duplication or
+ *  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+ */
+
+
+/**
+ *  WL.Server.invokeCastIron(parameters) accepts the following json object as an argument:
+ *  
+ *  {
+ *  	// Mandatory 
+ *  	method : 'get' or 'post', 
+ *  	path: value,
+ *  	appName: value,
+ *  
+ *  	// Optional 
+ *      requestType: 'http',
+ *  	returnedContentType: any known mime-type or one of "json", "css", "csv", "javascript", "plain", "xml", "html"  
+ *  	returnedContentEncoding : 'encoding', 
+ *  	parameters: {name1: value1, ... }, 
+ *  	headers: {name1: value1, ... }, 
+ *  	cookies: {name1: value1, ... }, 
+ *  	body: { 
+ *  		contentType: 'text/xml; charset=utf-8' or similar value, 
+ *  		content: stringValue 
+ *  	}, 
+ *  	transformation: { 
+ *  		type: 'default', or 'xslFile', 
+ *  		xslFile: fileName 
+ *  	} 
+ *  } 
+ */
+
+// orchestrationName ==> "ADFlowContact";
+
+function startOrchestration(orchestrationName){
+	
+	var input = {
+			method  : 'get',
+			appName : 'myApp',
+			requestType: 'http',
+			path : orchestrationName,			
+			returnedContentType : 'json'
+			
+	};
+	return WL.Server.invokeCastIron(input);
+}
+
+
+function startOrchestrationFiltered(orchestrationName){
+	
+	var input = {
+			method  : 'get',
+			appName : 'myApp',
+			requestType: 'http',
+			path : orchestrationName,			
+			returnedContentType : 'xml',
+			transformation : {
+				type : 'xslFile',
+				xslFile : 'ci-filtered.xsl'
+			}	
+			
+	};
+	return WL.Server.invokeCastIron(input);
+}
---
+++ ADFlowOffice/adapters/CastIronAdapter/ci-filtered.xsl
@@ -0,0 +1,20 @@
+<!--
+
+    IBM Confidential OCO Source Materials
+
+    5725-I43 Copyright IBM Corp. 2006, 2013
+
+    The source code for this program is not published or otherwise
+    divested of its trade secrets, irrespective of what has
+    been deposited with the U.S. Copyright Office.
+
+-->
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:h="http://www.w3.org/1999/xhtml">
+    <xsl:output method="text"/>
+    
+<!--  See http://www.w3.org/TR/xslt for additional information about XSL transformations -->
+  
+
+</xsl:stylesheet>
