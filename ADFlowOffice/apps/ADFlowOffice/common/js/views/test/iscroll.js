a5579f702fee4ce94ba9b9a7d497f4e2094a68b5
nadir93 <nadir93@gmail.com>
2014-01-03 16:33:40 +0900

    한글 폰트 테스트

--- ADFlowOffice/apps/ADFlowOffice/common/js/views/test/iscroll.js
+++ ADFlowOffice/apps/ADFlowOffice/common/js/views/test/iscroll.js
@@ -2,6 +2,11 @@ var lists = [ 'amy_jones', 'eugene_lee', 'gary_donovan', 'james_king',
 		'john_williams', 'julie_taylor', 'kathleen_byrne', 'lisa_wong',
 		'paul_jones', 'paula_gates', 'ray_moore', 'steven_wells' ];
 
+var names = [ '고신규', '권근필', '김은주', '조광일', '박택영', '최순옥', '이찬호', '박수정', '김병상',
+		'오형은', '양석모', '박눌' ];
+
+var titles = [ '대표', '차장', '과장', '사원', '대리', '부장', '상무', '전무', '이사' ];
+
 ADF.view.Iscroll = Backbone.View
 		.extend({
 			el : $('.panel-content'), // attaches `this.el` to an existing
@@ -26,12 +31,17 @@ ADF.view.Iscroll = Backbone.View
 									for (i = 0; i < 100; i++) {
 										str += '<li id="'
 												+ i
-												+ '" style="border-color:grey;border-style:solid none none none;border-width:1px;height:81px;background-color:white;"><div style="color:grey;"><img width=80 height=80 src="images/'
+												+ '" style="overflow: hidden;border-color:grey;border-style:solid none none none;border-width:1px;height:81px;background-color:white;"><div style="color:grey;float:left"><img width=80 height=80 src="images/'
 												+ lists[Math.floor(Math
 														.random() * 10)]
-												+ '.jpg">'
-												+ '<span style="align:top;">phone : +1040269329</span>'
-												+ '</div></li>';
+												+ '.jpg"></div>'
+												+ '<div style="color:grey;float:left"><h3 style="padding-left:10px;font-family:NanumGothicBold;">'
+												+ names[Math.floor(Math
+														.random() * 10)]
+												+ '</h3><p style="padding-left:12px;font-family:NanumGothicBold;">'
+												+ titles[Math.floor(Math
+														.random() * 9)]
+												+ '</p></div><div style="position:absolute;right:10px;color:grey;float:right;margin-top:30px;"><i class="icon-circle-arrow-right" /></div></li>';
 									}
 									$('#thelist').append(str);
 									// $('.back').on('click', function() {
